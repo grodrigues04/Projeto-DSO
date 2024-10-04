e384e1099f462a59983aa8544db4cfe4f951faaa
aquillesf <aquilleslerman@gmail.com>
2024-10-04 04:24:07 -0300

    adicionar catálogo

--- catalogo.py
+++ catalogo.py
@@ -1,3 +1,21 @@
+from jogo import Jogo
+
 class Catalogo():
-    pass
+    def __init__(self, jogo: Jogo, jogos_disponiveis:list=[]) -> None:
+        self.__jogos_disponiveis = []
+        self.__jogo = None
+
+        if isinstance(jogo, Jogo):
+            self.__jogo = Jogo
+
+    @property
+    def jogos_disponiveis(self):
+        return self.__jogos_disponiveis
+
+    @jogos_disponiveis.setter
+    def jogos_disponiveis(self, Jogo):
+        guardar_jogo = [Jogo]
+        self.__jogos_disponiveis.append(Jogo)
+        return "Jogo adicionado com sucesso ao catalogo!"
+
     
