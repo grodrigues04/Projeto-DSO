f81773e7de3d316ddc1a728170a1990d9e91447e
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-02 19:06:04 -0300

    feat: adicao do arquivo main.py para iniciar o sistema

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from model.usuario import Usuario
-from model.jogador import Jogador
-from model.jogo import Jogo
-from model.catalogo import Catalogo
-from model.desenvolvedor import Desenvolvedor
-jogo1 = Jogo("joao", "AO2", "dev32", "fps", "a")
+from controller.sistema_controller import ControladorSistema
+
+ControladorSistema().cadastra_usuario()
