dcffeda0bc136b1db91bb1ca35471e2130c2487b
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-04 19:18:13 -0300

    fix: correcao da compra de jogos

--- controller/jogador_controller.py
+++ controller/jogador_controller.py
@@ -4,7 +4,7 @@ class JogadorController(UsuarioController): #TEM QUE TIRAR O SELF DO CONTROLADOR
     def __init__(self, controlador_sistema) -> None:
         super().__init__(controlador_sistema)
         self.__jogadores = []
-        #self.__controlador_sistema = controlador_sistema
+        self.__controlador_sistema = controlador_sistema
         self.__tela_jogador = TelaJogador()
 
     @property
@@ -19,23 +19,24 @@ class JogadorController(UsuarioController): #TEM QUE TIRAR O SELF DO CONTROLADOR
 
     def comprar_jogo(self):
         jogos_controler = self.__controlador_sistema.jogo_controler
-        print(jogos_controler)
         repositorio = jogos_controler.repositorio_de_jogos
-        print(repositorio)
         jogo_desejado = self.__tela_jogador.adquirir_jogo(repositorio)
+        
         jogador_objeto = self.__controlador_sistema.sessao_atual
         biografia = jogador_objeto.biografia
         idade_minima = jogo_desejado.idade_minima
         if len(biografia) > 10:
             if idade_minima >= jogo_desejado.idade_minima:
+                print("OBJETO JOGADOR:",jogador_objeto)
                 #Adiciona o jogo na biblioteca do objeto jogador e a lista de jogadores ativos do objeto Jogo
                 jogador_objeto.adquirir_jogo(jogo_desejado)
-                jogador_objeto.adicionar_jogador(jogador_objeto)
+                jogo_desejado.adicionar_jogador(jogador_objeto)
+                self.iniciar_tela()
             else:
-                return False, "Idade minima insuficiente"
+                return "Idade minima insuficiente"
 
         else:
-            return False, "Sua biografia precisa de mais de 10 caracteres para poder adquirir um jogo"
+            return "Sua biografia precisa de mais de 10 caracteres para poder adquirir um jogo"
 
     def biblioteca_do_jogador(self):
         sessao_atual = self.__controlador_sistema.sessao_atual
@@ -43,11 +44,21 @@ class JogadorController(UsuarioController): #TEM QUE TIRAR O SELF DO CONTROLADOR
         self.__tela_jogador.mostrar_jogos(jogos)
     
     def iniciar_tela(self):
-        acoes = {
-            1:self.comprar_jogo,
-            2:self.biblioteca_do_jogador,
-            3:self.abrir_tela_de_perfil,
-        }
-        opcao = self.__tela_jogador.tela_opcoes()
-        funcao = acoes[opcao]
-        funcao()
+        while True: 
+            acoes = {
+                1: self.comprar_jogo,
+                2: self.biblioteca_do_jogador,
+                3: self.abrir_tela_de_perfil,
+                4: self.sair  
+            }
+            
+            opcao = self.__tela_jogador.tela_opcoes()
+            
+            funcao = acoes[opcao]
+            resposta = funcao()
+            
+            if isinstance(resposta,str):
+                self.__tela_jogador.msg(resposta)  # Executa a função correspondente à opção escolhida
+            
+            if opcao == 4:  # Número correspondente à opção de sair
+                break  # Encerra o loop para sair do menu
