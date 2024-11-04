f9bd3d8ddbc23918a5179586cf5ebd6ecd87ea15
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-03 21:03:01 -0300

    fix: mudança na identação das funções

--- model/jogo.py
+++ model/jogo.py
@@ -1,85 +1,86 @@
 class Jogo():
     #inicializadores
-    def __init__(self, titulo:str, autor:str, genero:str, biografia_jogo:str= "Sem biografia ainda", JogadoresAtivos:list=[]) -> None:
-        
+    def __init__(self, titulo:str, autor:str, genero:str,armazenamento:str, biografia_jogo:str) -> None:
         #inicializa os atributos
+        self.__armazenamento = armazenamento
         self.__autor = None
         self.__genero = None
         self.__biografia_jogo = None 
         self.__titulo = None
-        self.__JogadoresAtivos = JogadoresAtivos
+        self.__JogadoresAtivos = []
         
         #instancializa e verifica os tipos dos atributos que foram inicializados acima
-        if isinstance(autor,str):
+        if isinstance(autor, str):
             self.__autor = autor
             
-        if isinstance(genero,str):
+        if isinstance(genero, str):
             self.__genero = genero
             
-        if isinstance(biografia_jogo,str):
+        if isinstance(biografia_jogo, str):
             self.__biografia = biografia_jogo
             
-        if isinstance(titulo,str):
+        if isinstance(titulo, str):
             self.__titulo = titulo
                        
-        #inicio das funções da classe
-            
-        #getter e setter de autor
-        @property
-        def autor(self):
-            return self.__autor
+    @property
+    def autor(self):
+        return self.__autor
 
-        @autor.setter
-        def autor(self,autor):
-            if isinstance(autor,str):
-                self.__autor = autor
-                return "Seu autor foi alterado!"
-            else:
-                return "Autor incorreto."
-            
-        #getter e setter de genero
-        @property
-        def genero(self):
-            return self.__genero
-
-        @genero.setter
-        def genero(self,genero):
-            if isinstance(genero,str):
-                self.__genero = genero
-                return "O genero do jogo foi alterado!"
-            else:
-                return "Genero incorreto."
-            
-        #getter e setter de biografia_jogo
-        @property
-        def biografia_jogo(self):
-            return self.__biografia_jogo
-
-        @biografia_jogo.setter
-        def biografia_jogo(self,biografia_jogo):
-            if isinstance(biografia_jogo,str):
-                self.__biografia_jogo = biografia_jogo
-                return "A biografia do jogo foi alterada!"
-            else:
-                return "Biografia incorreta."
-            
-        #getter e setter de titulo
-        @property
-        def titulo(self):
-            return self.__titulo
-
-        @titulo.setter
-        def titulo(self,titulo):
-            if isinstance(titulo,str):
-                self.__titulo = titulo
-                return "O titulo do jogo foi alterado!"
-            else:
-                return "titulo incorreto."
+    @autor.setter
+    def autor(self,autor):
+        if isinstance(autor,str):
+            self.__autor = autor
+            return "Seu autor foi alterado!"
+        else:
+            return "Autor incorreto."
         
-        #getter da lista de jogadores
-        @property
-        def JogadoresAtivos(self):
-            return self.__JogadoresAtivos
+    #getter e setter de genero
+    @property
+    def genero(self):
+        return self.__genero
+
+    @genero.setter
+    def genero(self,genero):
+        if isinstance(genero,str):
+            self.__genero = genero
+            return "O genero do jogo foi alterado!"
+        else:
+            return "Genero incorreto."
+
+    @property
+    def biografia_jogo(self):
+        return self.__biografia_jogo
+
+    @biografia_jogo.setter
+    def biografia_jogo(self,biografia_jogo):
+        if isinstance(biografia_jogo,str):
+            self.__biografia_jogo = biografia_jogo
+            return "A biografia do jogo foi alterada!"
+        else:
+            return "Biografia incorreta."
+        
+    #getter e setter de titulo
+    @property
+    def titulo(self):
+        return self.__titulo
+    
+    @titulo.setter
+    def titulo(self,titulo):
+        if isinstance(titulo,str):
+            self.__titulo = titulo
+            return "O titulo do jogo foi alterado!"
+        else:
+            return "titulo incorreto."
+        
+    @property
+    def armazenamento(self):
+        return self.__armazenamento
+
+    
+    #getter da lista de jogadores
+    @property
+    def JogadoresAtivos(self):
+        return self.__JogadoresAtivos
 
         #eu n sei se agente vai precisar de um setter pra lista de jogadore ativos, pq eu n botei setter no diagrama e n me lembro se precisava
         #mas por precaução, vou deixar o código pra adicionar jgoadores aqui, eu tb n sei se herdaremos da classe Jogador ou da classe Usuario
