bcf5a4a6183d350a013f75b8b0d0d9cf57360e61
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-04 01:30:59 -0300

    fix: adicao de inputs para criacao de objeto jogo

--- view/tela_jogo.py
+++ view/tela_jogo.py
@@ -8,22 +8,26 @@ class TelaJogo():
         print(" --- Criando seu jogo --- ")
         titulo = input("Digite o titulo do jogo: ")
         genero = input("Digite o genero do jogo: ")
-        descricao  = input("Qual a descricao do jogo? ")
+        descricao  = input("Qual a descricao do jogo?")
+        idade = int(input("Digite a idade minima para jogar o jogo"))
         armazenamento = input("Espaço de armazenamento necessario: ")
         print()
         return {"titulo":titulo,
                 "autor":dev.nome_de_usuario,
                 "genero":genero,
-                "descricao":descricao, 
+                "descricao":descricao,
+                "idade_minima":idade,
                 "armazenamento":armazenamento
                 }
     
     def listar_repositorio(self, repositorio):
+        print()
         if len(repositorio) > 0:
             for jogo in repositorio:
                 print(jogo.titulo)
                 print(jogo.biografia_jogo)
                 print(jogo.armazenamento)
                 print(" ------------- ")
+                print()
         else:
             print("Não há jogos disponiveis no momento. Tente mais tarde")
